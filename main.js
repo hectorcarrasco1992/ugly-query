d077c7006e51dc16a3901512198dab74ddafc909
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-08 15:37:19 -0400

    made background image and changed colors

---
+++ main.js
@@ -0,0 +1,11 @@
+const pepe = document.querySelector('body')
+pepe.style.backgroundImage= 'url("https://i.imgur.com/JlXtPpf.png")'
+
+const uglyTitle = document.querySelector("h1")
+uglyTitle.style.fontFamily = "pt sans"
+uglyTitle.style.color = "yellow"
+
+
+const paragraph = document.querySelector('p')
+paragraph.style.fontSize= " 100px"
+paragraph.style.textAlign ='Center'
