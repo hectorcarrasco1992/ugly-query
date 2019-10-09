98f9a360dd4e8ccf71d92c02ecc8073552b2e45e
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-08 21:07:52 -0400

    added image to input and changed size of h3

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const pepe = document.querySelector('body')
 pepe.style.backgroundImage= 'url("https://i.imgur.com/JlXtPpf.png")'
 
+
 const uglyTitle = document.querySelector("h1")
 uglyTitle.style.fontFamily = "pt sans"
 uglyTitle.style.color = "yellow"
@@ -9,3 +10,24 @@ uglyTitle.style.color = "yellow"
 const paragraph = document.querySelector('p')
 paragraph.style.fontSize= " 100px"
 paragraph.style.textAlign ='Center'
+paragraph.style.color = " orange"
+
+
+const uglyList = document.querySelector('ul')
+uglyList.style.color = "hotpink"
+uglyList.style.border= "55px solid hotpink"
+uglyList.style.margin ="0px"
+uglyList.style.fontWeight = 'bold'
+
+
+const input1 = document.querySelector('form')
+input1.style.backgroundColor = "red"
+input1.style.margin = "100px"
+input1.style.borderWidth="200px"
+input1.style.borderImage = "url('https://media.giphy.com/media/urwZAV6l7XnYA/source.gif')"
+
+
+const newH3 =document.querySelector('#heading3')
+newH3.style.color = "yellow"
+newH3.style.fontSize = "100px"
+newH3.style.fontSize = "comic sans ms"
